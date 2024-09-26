22c97e8a65b65954adb969e7fbf21f05c686800c
Srikar Rao <srikar.rao@codezeros.com>
2024-09-26 12:06:47 +0530

    second task

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
-use std::{fs::File, io::{self, prelude::*, BufReader, BufWriter, Write}, thread::{sleep, spawn}, time::Duration};
+use std::{clone, fs::File, io::{self, prelude::*, BufReader, BufWriter, Write}, sync, thread::{self, sleep, spawn}, time::{Duration, Instant}};
 use clap::{builder::Str, Parser};
-use serde::{Deserialize, Serialize};
+use serde::{de::value, Deserialize, Serialize};
 use tungstenite::{accept, connect, WebSocket};
 use tokio_tungstenite::{connect_async, WebSocketStream, MaybeTlsStream};
 use tokio_tungstenite::tungstenite::protocol::Message;
+use tokio::task;
 // use args::{Args, ArgsError};
-use tokio::net::{TcpStream, TcpListener};
+use tokio::{net::{TcpListener, TcpStream}, sync::mpsc, time};
 use futures_util::{StreamExt};
 
 #[derive(Parser,Debug)]
@@ -73,7 +74,7 @@ async fn cache_mode(times: u32) {
     //         Some(Err(e)) => {
     //             eprintln!("Error receieving the message {e}")
     //         }
-    //         None => {
+    //         None => {6
     //             println!("Websocket closed");
     //             break;
     //         }
@@ -104,12 +105,40 @@ fn extract_usd_btc_price(msg: &str) -> f64 {
 
 #[tokio::main]
 async fn main() {
-    let mut args = Args::parse();
+    let args = Args::parse();
     match args.mode.as_str() {
         "cache" => cache_mode(args.times).await,
         "read"  => read_mode(),
         _ => println!("Invalid mode. Use --mode=cache or --mode=read")
     }
+
+    let buffer:usize = 10;
+
+    let (tx, rx) = mpsc::channel(buffer);
+    let mut handles = vec![];
+
+    for i in 0..5 {
+        let tx_clone = tx.clone();
+        let handle = task::spawn(async move {
+            client_process(i, tx_clone).await;
+        });
+        handles.push(handle);
+    }
+
+    drop(tx);
+
+    let aggregator_process = task::spawn(async move {
+        aggregator_process(rx).await
+    });
+
+    for handle in handles {
+        handle.await.unwrap();
+    }
+
+    aggregator_process.await.unwrap();
+
+    println!("All processes completed")
+    
 
 }
 
@@ -124,6 +153,47 @@ fn read_mode() {
 
 }
 
+async fn client_process(client_id: usize, tx: mpsc::Sender<f64>) {
+    println!("The client {} is getting started at {:?}", client_id, chrono::Local::now());
+    
+    let start_time = Instant::now();
+    let mut values: Vec<f64> = Vec::new();
+
+    while start_time.elapsed() < Duration::new(10, 0) {
+        let value = rand::random::<f64>() * 100.0;
+        values.push(value);
+        println!("The client {} has provided the value: {:?}", client_id, value);
+        break;
+    }
+    sleep(Duration::from_secs(1));
+
+    let average = if !values.is_empty() {
+        values.iter().sum::<f64>() / values.len() as f64
+    } else {
+        0.0
+    };
+    println!("The client provided an average value : {:?}", average);
+    tx.send(average).await.unwrap();
+}
+
+async fn aggregator_process(mut rx: mpsc::Receiver<f64>) {
+    println!("The aggregator is waiting for client averages.....");
+
+    let mut averages: Vec<f64> = Vec::new();
+
+    for i in 0..5 {
+        let average = rx.recv().await.unwrap();
+        averages.push(average);
+        println!("The received average from the client is {:?}", average)
+    }
+
+    let overall_averages: f64 = if !averages.is_empty() {
+        averages.iter().sum::<f64>() / averages.len() as f64
+    } else {
+        0.0
+    };
+    println!("The aggregator computed overall averages from the client is {:?}", overall_averages )
+}
 // fn main() {
 //     let server = TcpListener::bind("127.0.0.1:9001").unwrap();
 
